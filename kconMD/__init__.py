bec4a6adb13ee9fbcbc32717ddbc79c7292ce16f
Jinzhe Zeng <njzjz@qq.com>
2018-08-22 12:28:33 +0800

    unit

--- kconMD/__init__.py
+++ kconMD/__init__.py
@@ -1,18 +1,20 @@
 from kconMD.force import ComputeForces
 import time
 class kconMD(object):
-    def __init__(self,pbfilename,xyzfilename,outputfilename,cell=[0,0,0],pbc=True,cutoff=6):
+    def __init__(self,pbfilename,xyzfilename,outputfilename,cell=[0,0,0],pbc=True,cutoff=6,unit=1):
         self.pbfilename=pbfilename
         self.xyzfilename=xyzfilename
         self.cell=cell
         self.pbc=pbc
         self.cutoff=cutoff
         self.outputfilename=outputfilename
+        self.unit=unit
 
     def printforce(self):
         time1=time.time()
         cf=ComputeForces(self.pbfilename,cell=self.cell,pbc=self.pbc,cutoff=self.cutoff)
         forces=cf.predictforcesfromxyz(self.xyzfilename)
+        forces*=unit
         with open(self.outputfilename,'w') as f:
             for force in forces:
                 print("".join("%16.9f"%x for x in force),file=f)
