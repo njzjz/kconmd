e2801a5cfd124a905e7b4c050483415fab4d3741
Jinzhe Zeng <njzjz@qq.com>
2018-09-27 16:32:38 +0800

    update my email address

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 setup(name='kconMD',
-      version='1.0.8',
+      version='1.0.9',
       description='Molecular dynamics (MD) simulations supported by k-Bags Convolutional Neural Network (kcon).',
       keywords="molecular dynamics kcon",
       url='https://github.com/njzjz/kconMD',
       author='Jinzhe Zeng',
-      author_email='njzjz@qq.com',
+      author_email='jzzeng@stu.ecnu.edu.cn',
       packages=['kconMD','kconMD/kconMD','kconMD/force','kconMD/kcnn','kconMD/server'],
       install_requires=['numpy','scipy','matplotlib','scikit-learn','ase>=3.12'],
       extras_require={
